68d94dc06ecfe98f8c6824248cc4063aff4e043b
Motikoni Mohofe <motikonisimon@gmail.com>
2025-09-11 22:46:02 +0200

    mounted book routes

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import logger from "./middleware/logger";
+import authorRouter from "./routes/authors";
+import booksRouter from "./routes/books";
+
+const app = express()
+
+app.use(express.json())
+app.use(logger)
+
+app.get("/", (req, res) => {
+    res.json({message: "Library API"})
+})
+
+app.use("/authors", authorRouter)
+app.use("/books", booksRouter)
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`)
+})
