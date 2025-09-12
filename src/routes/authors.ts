2cdf1bac870bd4a7f1f481f51bad83bd8ac1b1b9
Motikoni Mohofe <motikonisimon@gmail.com>
2025-09-12 23:43:20 +0200

    completed sprint 4

--- src/routes/authors.ts
+++ src/routes/authors.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from "express";
 import { authors, generateAuthorId } from "../data/authors";
 import { Author } from "../models/author";
 import { error } from "console";
+import { AppError } from "../utils/AppError";
+import { books } from "../data/books";
 
 const router = express.Router()
 
@@ -34,7 +36,9 @@ router.get("/:id", (req: Request, res: Response) => {
     }
 
     const author = authors.find(a => a.id === id)
-    if(!author) return res.status(404).json({error: "Author not found."})
+    if(!author){
+        return res.status(404).json({error: "Author not found."})
+    }
     
     const {name, birthYear, bio} = req.body
     
@@ -62,6 +66,24 @@ router.delete("/:id", (req: Request, res: Response) => {
 
     authors.splice(idx, 1)
     return res.status(204).end()
+})
+
+router.get("/:id/books", (req, res, next) => {
+    try {
+        const authorId = Number(req.params.id)
+        if(Number.isNaN(authorId)) {
+            throw new AppError("Invalid author id.", 400)
+        }
+
+        const authorBooks = books.filter(b => b.authorId === authorId)
+        if(authorBooks.length === 0) {
+            return res.json([])
+        }
+
+        return res.json(authorBooks)
+    } catch (err) {
+        next(err)
+    }
 })
 
 export default router;
